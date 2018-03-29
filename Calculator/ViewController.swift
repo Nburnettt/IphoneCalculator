3fa9a5f16426e10f5c292ac31b85222545ddef70
Nathan Burnett <nathanburnett96@yahoo.com>
2018-03-28 18:01:28 -0700

    Initial Commit - Basic Calculator Functionality Working

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,20 +6,59 @@
 //  Copyright © 2018 Nathan Burnett. All rights reserved.
 //
 
+import Expression
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var userIsTyping = false
+    var linesOfCalculations: [String] = ["", "", "", "", "", "", "", "", "", ""]
+    
+    @IBOutlet weak var calculationsDisplay: UILabel!
+    @IBOutlet weak var display: UILabel!
+    
+    private func updateCalculationsDisplay() {
+        calculationsDisplay.text = ""
+        while linesOfCalculations.count > 9 {
+            linesOfCalculations.removeFirst()
+        }
+        for line in linesOfCalculations{
+            if calculationsDisplay.text! != "" {
+                calculationsDisplay.text! += "\n"
+            }
+            calculationsDisplay.text! += line
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchEvaluate() {
+        let textInDisplay = display.text!
+        var result = ""
+        display.text = ""
+        let expression = Expression(textInDisplay)
+        do{
+            result = String(try expression.evaluate())
+        } catch {
+            result = "Error"
+        }
+        linesOfCalculations.append(textInDisplay)
+        linesOfCalculations.append(result)
+        updateCalculationsDisplay()
     }
-
-
+    
+    @IBAction func touchOperator(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        display.text! += operation
+    }
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textInDisplay = display.text!
+        if userIsTyping {
+            display.text = textInDisplay + digit
+        } else {
+            display.text = digit
+            userIsTyping = true
+        }
+    }
 }
 
