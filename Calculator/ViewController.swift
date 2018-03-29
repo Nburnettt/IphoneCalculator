c15b34643fcdef01338139710147fdc8f4174f6c
Nathan Burnett <nathanburnett96@yahoo.com>
2018-03-28 20:53:21 -0700

    More features working.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,23 +11,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var userIsTyping = false
-    var linesOfCalculations: [String] = ["", "", "", "", "", "", "", "", "", ""]
-    
-    @IBOutlet weak var calculationsDisplay: UILabel!
+    var lines: [NSMutableAttributedString] = [NSMutableAttributedString(), NSMutableAttributedString()]
     @IBOutlet weak var display: UILabel!
     
+    @IBAction func touchPreviousCalculation(_ sender: UIButton) {
+        
+    }
+
     private func updateCalculationsDisplay() {
-        calculationsDisplay.text = ""
-        while linesOfCalculations.count > 9 {
-            linesOfCalculations.removeFirst()
-        }
-        for line in linesOfCalculations{
-            if calculationsDisplay.text! != "" {
-                calculationsDisplay.text! += "\n"
+        let numCalculationLines = 7
+        let indexOfFirstLine = 10
+        let indexOfLastLine = indexOfFirstLine + numCalculationLines - 1
+        
+        let newCalculation = lines[1]
+        let newResult = lines[0]
+        
+        var prev = NSAttributedString()
+        
+        for i in stride(from: indexOfLastLine, through: indexOfFirstLine, by: -1) {
+            let currentButton = self.view.viewWithTag(i) as? UIButton
+            if i - 2 >= indexOfFirstLine{
+                let tmpButton = self.view.viewWithTag(i - 2) as! UIButton
+                prev = tmpButton.attributedTitle(for: .normal)!
+                currentButton?.setAttributedTitle(prev, for: .normal)
             }
-            calculationsDisplay.text! += line
         }
+        let secondButton = self.view.viewWithTag(indexOfFirstLine + 1) as? UIButton
+        secondButton?.setAttributedTitle(newCalculation, for: .normal)
+        let firstButton = self.view.viewWithTag(indexOfFirstLine) as? UIButton
+        firstButton?.setAttributedTitle(newResult, for: .normal)
+    }
+    
+    private func createColoredString(_ myString: String, myColor: UIColor) -> NSMutableAttributedString {
+        let myRange = NSMakeRange(0, myString.count)
+        let attributedString = NSMutableAttributedString(string: myString)
+        attributedString.addAttribute(NSAttributedStringKey.foregroundColor, value: myColor, range: myRange)
+        return attributedString
     }
     
     @IBAction func touchEvaluate() {
@@ -40,8 +59,8 @@ class ViewController: UIViewController {
         } catch {
             result = "Error"
         }
-        linesOfCalculations.append(textInDisplay)
-        linesOfCalculations.append(result)
+        lines[1] = createColoredString(textInDisplay, myColor: UIColor.lightGray)
+        lines[0] = createColoredString(result, myColor: UIColor.green)
         updateCalculationsDisplay()
     }
     
@@ -53,12 +72,7 @@ class ViewController: UIViewController {
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         let textInDisplay = display.text!
-        if userIsTyping {
-            display.text = textInDisplay + digit
-        } else {
-            display.text = digit
-            userIsTyping = true
-        }
+        display.text = textInDisplay + digit
     }
 }
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '10.0'
+
+target 'Calculator' do
+  use_frameworks!
+
+  pod 'Expression', '~> 0.12'
+
+end
